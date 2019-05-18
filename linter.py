106ab940ae0c282bc6ec7a5ddfb310b6b16d249f
EdrisT <47713852+EdrisT@users.noreply.github.com>
2019-05-18 22:13:39 +0200

    Update linter.py

--- linter.py
+++ linter.py
@@ -22,9 +22,9 @@ function PSLint
 class Powershell(Linter):
 
     if os.name == 'nt':
-        cmd = 'powershell.exe -nol -c {}'.format(script) + '; PSLint -PSFile ${file}'
+        cmd = 'powershell.exe -nol -c {}'.format(script) + '; PSLint -PSFile ${temp_file}'
     else:
-        cmd = 'pwsh -nol -c {}'.format(script) + '; PSLint -PSFile ${file}'
+        cmd = 'pwsh -nol -c {}'.format(script) + '; PSLint -PSFile ${temp_file}'
 
     regex = (r'(?P<line>\d+)\s\s\s(?P<message>.*?)\s\s\s((?P<error>ParseError)|(?P<warning>Warning|Information))\s\s\s(?P<col>\d+)\s\s\s(?P<code>.*)')
     tempfile_suffix = '.tempPS1'
