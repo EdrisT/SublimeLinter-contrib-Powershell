d562303c7cd7438a58f738cf86ed2ebc0a19a2d0
edris.tavoosi.6887@gmail.com <47713852+EdrisT@users.noreply.github.com>
2019-05-19 10:48:11 +0200

    small fix

--- linter.py
+++ linter.py
@@ -22,7 +22,9 @@ class Powershell(Linter):
     else:
         cmd = 'pwsh -nol -c {}'.format(script) + '; PSLint -PSFile ${temp_file}'
 
-    regex = (r'(?P<line>\d+)\s\s\s(?P<message>.*?)\s\s\s((?P<error>\S*Error)|(?P<warning>Warning|Information))\s\s\s(?P<col>\d+)\s\s\s(?P<code>.*)')
+    regex = (
+        r'(?P<line>\d+)\s\s\s(?P<message>.*?)\s\s\s((?P<error>\S*Error)'
+        r'|(?P<warning>Warning|Information))\s\s\s(?P<col>\d+)\s\s\s(?P<code>.*)')
     tempfile_suffix = '.tempPS1'
     multiline = False
     word_re = r'^([-\S]+|\s+$)'
