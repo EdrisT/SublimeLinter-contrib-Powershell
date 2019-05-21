648410644c988db4405c2ee9ab25ac3aafd977b1
Edris <edris.tavoosi.6887@gmail.com>
2019-05-21 19:33:55 +0200

    Added option to use custom ScriptAnalyzer settings
    1. Added 2 different ways to use custom ScriptAnalyzer settings.
    2. The regex now makes use of the "near" capture group from SublimeLinters regex attribute when possible for better error highlighting.
    3. Suggestions are now shown in the linter message when available.

--- linter.py
+++ linter.py
@@ -3,33 +3,58 @@ import os
 
 script = """
 Import-Module PSScriptAnalyzer;
-"\\$FullResult = Invoke-ScriptAnalyzer -Path ${temp_file}";
-"foreach (\\$Result in \\$FullResult)"
+if (Test-Path PSScriptAnalyzerSettings.psd1)
 {
-    "\\$Line = \\$Result.Line";
-    "\\$Message = \\$Result.Message";
-    "\\$RuleName = \\$Result.RuleName";
-    "\\$Severity = \\$Result.Severity";
-    "\\$Column = \\$Result.column";
-    '\"\\$Line   \\$Message   \\$Severity   \\$Column   \\$RuleName\"'
+    "\\$FullResult" = Invoke-ScriptAnalyzer ${temp_file} -Setting PSScriptAnalyzerSettings.psd1
+}
+else
+{
+    if ("\\$GlobalPSScriptAnalyzerSettingsPath")
+    {
+        "\\$FullResult" = Invoke-ScriptAnalyzer ${temp_file} -Setting "\\$GlobalPSScriptAnalyzerSettingsPath"
+    }
+    else
+    {
+        "\\$FullResult" = Invoke-ScriptAnalyzer -Path ${temp_file}
+    }
+};
+
+foreach ("\\$Result" in "\\$FullResult")
+{
+    "\\$Line"       =   "\\$Result.Line" ;
+    "\\$Message"    =   "\\$Result.Message" ;
+    "\\$RuleName"   =   "\\$Result.RuleName" ;
+    "\\$Severity"   =   "\\$Result.Severity" ;
+    "\\$Column"     =   "\\$Result.column" ;
+    "\\$Extent"     =   "\\$Result.Extent.Text.trim()" ;
+    "\\$Suggestion" =   "\\$Result.SuggestedCorrections.description" ;
+
+    if ("\\$Extent")
+    {
+        Write-Host '"Line:\\$Line RuleName:\\$RuleName Severity:\\$Severity Extent:\\$Extent Message:\\$Message \\$Suggestion"'
+    }
+    else
+    {
+        Write-Host '"Line:\\$Line RuleName:\\$RuleName Severity:\\$Severity Column:\\$Column Message:\\$Message \\$Suggestion"'
+    }
 }"""
 
 
 class Powershell(Linter):
 
     if os.name == 'nt':
-        cmd = 'powershell.exe -nol -c {}'.format(script)
+        cmd = 'powershell.exe -nol -c {}; '.format(script)
     else:
-        cmd = 'pwsh -nol -c {}'.format(script)
+        cmd = 'pwsh -nol -c {}; '.format(script)
 
     regex = (
-        r'(?P<line>\d+)\s\s\s(?P<message>.*?)\s\s\s((?P<error>\S*Error)|'
-        r'(?P<warning>Warning|Information))\s\s\s(?P<col>\d+)\s\s\s(?P<code>.*)'
+        r'Line:(?P<line>\d+)\sRuleName:(?P<code>\w+)\sSeverity:((?P<error>\S*?Error)|'
+        r'(?P<warning>Warning|Information))\s(Column:(?P<col>\d+)|Extent:(?P<near>.*?))\sMessage:(?P<message>.*)'
     )
 
-    tempfile_suffix = '.tempPS1'
+    tempfile_suffix = 'ps'
     multiline = False
     word_re = r'^([-\S]+|\s+$)'
     defaults = {
-        'selector': 'source.powershell'
+        "selector": "source.powershell"
     }
